e6b7df1d39e8fd700956eb62b1cbc75bfb94b956
moviefullframe <102091878+moviefullframe@users.noreply.github.com>
2022-03-22 00:49:56 +0300

    Add files via upload

---
+++ authorization/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace authorization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbName = new System.Windows.Forms.Label();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbName.Location = new System.Drawing.Point(29, 24);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(291, 100);
+            this.lbName.TabIndex = 0;
+            this.lbName.Text = "Cистема учета электронного\r\n         документооборота \r\n\r\n\r\n";
+            this.lbName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(73, 95);
+            this.tbLogin.MaxLength = 20;
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(225, 20);
+            this.tbLogin.TabIndex = 1;
+            this.tbLogin.Text = "логин";
+            this.tbLogin.TextChanged += new System.EventHandler(this.tbLogin_TextChanged);
+            this.tbLogin.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbLogin_KeyDown);
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(73, 136);
+            this.tbPass.MaxLength = 20;
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(225, 20);
+            this.tbPass.TabIndex = 2;
+            this.tbPass.Text = "пароль";
+            this.tbPass.TextChanged += new System.EventHandler(this.tbPass_TextChanged);
+            this.tbPass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPass_KeyDown);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(73, 195);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "Войти";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(223, 195);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Отмена ";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 230);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.lbName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ authorization/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace authorization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbLogin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form message = new Form();
+            MessageBox.Show("Ваш логин:" + tbLogin.Text + "\nВаш пароль:" + tbPass.Text);
+        }
+
+        private void tbPass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            KeysComb(e, tbLogin.Text);
+        }
+
+        private void tbPass_KeyDown(object sender, KeyEventArgs e)
+        {
+            KeysComb(e, tbPass.Text);
+        }
+        private void KeysComb(KeyEventArgs e, String tb)
+        {
+            if (e.Alt && e.KeyCode == Keys.J)
+            {
+                MessageBox.Show(tb);
+            }
+
+
+        }
+    }
+}
